d6dd1563b8756074f4d9b6179e1d87718b2024bb
ayeshaa125789-lab <ayeshaa125789@gmail.com>
2025-10-23 21:10:02 +0500

    Update ai_translator_app.py

--- ai_translator_app.py
+++ ai_translator_app.py
@@ -32,7 +32,7 @@ st.set_page_configst.set_page_config(
 
 # ✅ Google Search Console verification
 st.markdown(
-    '<meta name="google-site-verification" content="ABC123XYZ456"/>',
+    '<meta name="google-site-verification" content="d1f2g3h4i5j6k7l8m9n0"/>',
     unsafe_allow_html=True
 )
 
@@ -157,3 +157,4 @@ if st.button("🚪 Logout"):
     st.session_state.logged_in = False
     st.session_state.username = ""
     st.rerun()
+
